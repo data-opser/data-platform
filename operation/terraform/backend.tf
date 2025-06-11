2f5141248916addb00cc8663eed712a89f64b2ff
NikitaG25 <nikita.holokha@nure.ua>
2025-06-11 15:53:57 +0300

    fix backend terraform

--- operation/terraform/backend.tf
+++ operation/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "gcs" {
-    bucket  = " us-central1-prod-airflow-8d2312da-bucket"
+    bucket  = "us-central1-prod-airflow-8d2312da-bucket"
     prefix  = "terraform/state.tfstate"
   }
 }
